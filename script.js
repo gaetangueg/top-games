ae696669fe25bcca143e884d85fa9b4c22a86ef4
gaetan <gaetan.guegan951@gmail.com>
2024-11-29 09:44:18 +0100

    modification de la position de submit

--- script.js
+++ script.js
@@ -165,11 +165,18 @@ function editModal(gameId) {
 
 
 
-function modifyFom(gameData) {
-	const form = document.querySelector("form")
-	form.title.value = gameData.title
-	form.year.value = gameData.year
-	form.imageUrl.value = gameData.imageUrl
+function modifyModal(modalTitle, modalBody) {
+	// Écrire le nom du jeu dans le titre du modal
+	document.querySelector(".modal-title").textContent = modalTitle
+	// Écrire dans le corps du modal
+	document.querySelector(".modal-body").innerHTML = modalBody
+	// Écrire dans le footer
+	document.querySelector(".modal-footer").innerHTML = `
+		<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+			Close
+		</button>
+		<button type="submit" data-bs-dismiss="modal" class="btn btn-primary">Submit</button>
+</form>`
 }
 
 
