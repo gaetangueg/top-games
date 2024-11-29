b293e8438ae361148bd6fa5b70cf9626ce4af1c3
gaetan <gaetan.guegan951@gmail.com>
2024-11-29 09:47:00 +0100

    test

--- script.js
+++ script.js
@@ -165,18 +165,11 @@ function editModal(gameId) {
 
 
 
-function modifyModal(modalTitle, modalBody) {
-	// Écrire le nom du jeu dans le titre du modal
-	document.querySelector(".modal-title").textContent = modalTitle
-	// Écrire dans le corps du modal
-	document.querySelector(".modal-body").innerHTML = modalBody
-	// Écrire dans le footer
-	document.querySelector(".modal-footer").innerHTML = `
-		<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-			Close
-		</button>
-		<button type="submit" data-bs-dismiss="modal" class="btn btn-primary">Submit</button>
-</form>`
+function modifyFom(gameData) {
+	const form = document.querySelector("form")
+	form.title.value = gameData.title
+	form.year.value = gameData.year
+	form.imageUrl.value = gameData.imageUrl
 }
 
 
